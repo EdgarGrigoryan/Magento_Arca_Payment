966024cd0e03baed2547ac61b31df31a2291e906
Web Department <web@studio-one.am>
2014-03-17 15:17:22 +0400

    Commit 2 Nothing to write yet

---
+++ app/code/local/Studioone/ArCa/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Studioone_ArCa_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/Studioone/ArCa/sql/pay_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,9 @@
+<?php
+$installer = $this;
+/* @var $installer Mage_Customer_Model_Entity_Setup */
+
+$installer->startSetup();
+
+$installer->run("");
+
+$installer->endSetup();
